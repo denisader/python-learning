87f31f299b805c1019f63fa90bea8c816b98a576
Denisa Dersedan <denisa.dersedan@analog.com>
2026-07-08 13:59:18 +0300

    Added a queue and a scheduler

--- concurrent-programming/wikipedia-reader/wikipedia-reader.py
+++ concurrent-programming/wikipedia-reader/wikipedia-reader.py
@@ -1,19 +1,29 @@
 import time
 
+from multiprocessing import Queue
+
 from workers.WikiWorker import WikiWorker
-from workers.YahooFinanceWorkers import YahooFinanacePriceWorker
+from workers.YahooFinanceWorkers import YahooFinancePriceScheduler
 
 def main():
+    symbol_queue = Queue()
     start_time = time.time()
 
     wikiWorker = WikiWorker()
-    current_workers = []
+    yahoo_scheduler_threads = []
+    num = 10
+    for i in range(num):
+        yahooFinancePriceScheduler = YahooFinancePriceScheduler(input_queue=symbol_queue)
+        yahoo_scheduler_threads.append(yahooFinancePriceScheduler)
+        
     for symbol in wikiWorker.get_sp_500_companies():
-        yahooFinanacePriceWorker = YahooFinanacePriceWorker(symbol=symbol)
-        current_workers.append(yahooFinanacePriceWorker)
+        symbol_queue.put(symbol)
 
-    for i in range(len(current_workers)):
-        current_workers[i].join()
+    for i in range(len(yahoo_scheduler_threads)):
+        symbol_queue.put('DONE')
+
+    for i in range(len(yahoo_scheduler_threads)):
+        yahoo_scheduler_threads[i].join()
 
     print('Extracting time took: ', round(time.time() - start_time, 1))
 
--- concurrent-programming/wikipedia-reader/workers/YahooFinanceWorkers.py
+++ concurrent-programming/wikipedia-reader/workers/YahooFinanceWorkers.py
@@ -4,24 +4,35 @@ from lxml import html
 import time
 import random
 
-class YahooFinanacePriceWorker(threading.Thread):
-    _semaphore = threading.Semaphore(10)
-
-    def __init__(self, symbol, **kwargs):
+class YahooFinancePriceScheduler(threading.Thread):
+    def __init__(self, input_queue, **kwargs):
         super().__init__(**kwargs)
+        self._input_queue = input_queue
+        self.start()
+    
+    def run(self):
+        while True:
+            val = self._input_queue.get()
+            if val == 'DONE':
+                break
+
+            yahooFinanacePriceWorke = YahooFinanacePriceWorker(symbol=val)
+            price = yahooFinanacePriceWorke.get_price()
+            print(price)
+            time.sleep(random.random())
+
+class YahooFinanacePriceWorker():
+    def __init__(self, symbol, **kwargs):
         self._symbol = symbol
         base_url = 'https://finance.yahoo.com/quote/'
         self._url = f'{base_url}{self._symbol}'
-        self.start()
 
-    def run(self):
-        with self._semaphore:
-            time.sleep(30 * random.random())
-            r = requests.get(self._url, headers={'User-Agent': 'Mozilla/5.0'})
-            if r.status_code != 200:
-                print(f'{self._symbol}: failed with status {r.status_code}')
-                return
-            page_contents = html.fromstring(r.text)
-            page_text = page_contents.xpath('//*[@id="main-content-wrapper"]/section[1]/div[2]/div[1]/section/div/section[1]/div[1]/span[1]')[0].text
-            price = float(page_text.replace(',', ''))
-            print(price)
+    def get_price(self):
+        r = requests.get(self._url, headers={'User-Agent': 'Mozilla/5.0'})
+        if r.status_code != 200:
+            print(f'{self._symbol}: failed with status {r.status_code}')
+            return
+        page_contents = html.fromstring(r.text)
+        page_text = page_contents.xpath('//*[@id="main-content-wrapper"]/section[1]/div[2]/div[1]/section/div/section[1]/div[1]/span[1]')[0].text
+        price = float(page_text.replace(',', ''))
+        return price
